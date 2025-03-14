5a4587b990046e2c4aedf246542cbe4b9e2658b8
cloudwalabanda <vinitandhare5@gmail.com>
2025-03-14 14:33:25 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Date currentDate = new Date();
-        System.out.println("Welcome To learn Docker! Current date: " + currentDate);
+        System.out.println("Welcome To Cloud Baby ! Current date: " + currentDate);
     }
 }
